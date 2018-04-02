874011d17ca4faf62e206da50a92ef4643418bc5
ATARI\buddygn <buddygn@gmail.com>
2018-04-02 20:40:56 -0300

    init

---
+++ Adocao/views.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+from django.http import HttpResponse
+from django.views.decorators.csrf import csrf_exempt
+
+
+@csrf_exempt
+def index(request):
+    if request.method == 'POST':
+        nome = request.POST.get('name',u'nao tem nome')
+        return HttpResponse(u'o nome é %s' %nome)
+    else:
+        formulario = '''
+        <form action="." method="post">
+            <input type="text" name="name" maxlength="100" />
+            <button type="submit">Enviar</button>
+        </form>
+        '''
+        return HttpResponse(formulario)
